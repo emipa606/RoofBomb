b0ee2715a34ce6d891f79126e6f09bf72dc5ff39
Mlie <emipa606@users.noreply.github.com>
2022-10-19 19:48:09 +0200

    Mod updated for 1.4 and passed autotests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # RoofBomb
 
-![Image](https://i.imgur.com/WAEzk68.png)
+![Image](https://i.imgur.com/buuPQel.png)
 
 Update of Skullywags mod
 https://steamcommunity.com/sharedfiles/filedetails/?id=731737035
@@ -10,14 +10,14 @@ https://steamcommunity.com/sharedfiles/filedetails/?id=1539350259
 - Will no longer leave resources when exploding
 - Research now has IEDs as requirement
 
-![Image](https://i.imgur.com/7Gzt3Rg.png)
+![Image](https://i.imgur.com/pufA0kM.png)
 
 	
-![Image](https://i.imgur.com/NOW7jU1.png)
+![Image](https://i.imgur.com/Z4GOv8H.png)
 
 Adds an explosive device capable of removing roofs. More than 1 may be required, will turn mountain roof to thin rock roof and removes thin rock roof.
 
-![Image](https://i.imgur.com/Rs6T6cr.png)
+![Image](https://i.imgur.com/PwoNOj4.png)
 
 
 
@@ -27,6 +27,7 @@ Adds an explosive device capable of removing roofs. More than 1 may be required,
 -  For best support, please use the Discord-channel for error-reporting.
 -  Do not report errors by making a discussion-thread, I get no notification of that.
 -  If you have the solution for a problem, please post it to the GitHub repository.
+
 
 
 
--- Source/RoofBomb/Building_RoofBomb.cs
+++ Source/RoofBomb/Building_RoofBomb.cs
@@ -3,42 +3,41 @@ using RimWorld;
 using UnityEngine;
 using Verse;
 
-namespace RoofBomb
+namespace RoofBomb;
+
+public class Building_RoofBomb : Building
 {
-    public class Building_RoofBomb : Building
+    public override IEnumerable<Gizmo> GetGizmos()
     {
-        public override IEnumerable<Gizmo> GetGizmos()
+        yield return new Command_Action
         {
-            yield return new Command_Action
-            {
-                defaultLabel = "CommandDetonateLabel".Translate(),
-                icon = ContentFinder<Texture2D>.Get("UI/Commands/Detonate"),
-                defaultDesc = "CommandDetonateDesc".Translate(),
-                action = Command_Detonate
-            };
+            defaultLabel = "CommandDetonateLabel".Translate(),
+            icon = ContentFinder<Texture2D>.Get("UI/Commands/Detonate"),
+            defaultDesc = "CommandDetonateDesc".Translate(),
+            action = Command_Detonate
+        };
+    }
+
+    private void Command_Detonate()
+    {
+        GetComp<CompExplosive>().StartWick();
+    }
+
+    public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+    {
+        var map = Map;
+        base.Destroy(mode);
+        FleckMaker.ThrowMicroSparks(DrawPos, map);
+        if (mode != DestroyMode.KillFinalize)
+        {
+            return;
         }
 
-        private void Command_Detonate()
+        foreach (var current in this.CellsAdjacent8WayAndInside())
         {
-            GetComp<CompExplosive>().StartWick();
-        }
-
-        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
-        {
-            var map = Map;
-            base.Destroy(mode);
-            FleckMaker.ThrowMicroSparks(DrawPos, map);
-            if (mode != DestroyMode.KillFinalize)
+            if (map.roofGrid.RoofAt(current) != null) // if there is a roof
             {
-                return;
-            }
-
-            foreach (var current in this.CellsAdjacent8WayAndInside())
-            {
-                if (map.roofGrid.RoofAt(current) != null) // if there is a roof
-                {
-                    map.roofGrid.SetRoof(current, null);
-                }
+                map.roofGrid.SetRoof(current, null);
             }
         }
     }
